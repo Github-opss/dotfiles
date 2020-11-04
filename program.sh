e09a255607821293a7f0b67ca4631fba0a04386e
Github-opss <54327724+Github-opss@users.noreply.github.com>
2020-11-04 00:40:19 -0500

    Update program.sh

--- program.sh
+++ program.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
+
+echo "Creating directories
+cd ~
+mkdir -p Downloads Documents Pictures Music 
+
 echo "Cloning git"
-git clone https://github.com/Github-opss/dotfiles.git ~/.config
-echo "Installing packages"
-dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi 
+git clone https://github.com/Github-opss/dotfiles.git
+
+echo "Configuring i3"
+mv i3 i3status kitty rofi ~/.config
+
 echo "Installing theme"
 mkdir -p ~/.themes && tar -C ~/.themes -xvf dracula.tar
+
+echo "Installing packages"
+dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi 
 
