752c8863670a41764eb6c1fff2e596e6a215607c
Github-opss <54327724+Github-opss@users.noreply.github.com>
2020-11-04 00:30:57 -0500

    Update program.sh

--- program.sh
+++ program.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+echo "Cloning git"
+git clone https://github.com/Github-opss/dotfiles.git ~/.config
+echo "Installing packages"
 dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi 
-
-#Installing theme
+echo "Installing theme"
 mkdir -p ~/.themes && tar -C ~/.themes -xvf dracula.tar
 
