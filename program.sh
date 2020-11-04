c1f150f2e159ecf5176a7b55277c0a0ece25893f
Github-opss <54327724+Github-opss@users.noreply.github.com>
2020-11-04 00:28:41 -0500

    Create program.sh

---
+++ program.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi 
+
+#Installing theme
+mkdir -p ~/.themes && tar -C ~/.themes -xvf dracula.tar
+
