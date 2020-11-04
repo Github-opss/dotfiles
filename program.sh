2bf2e0fa244c4604db27181c004768005f0eea2c
Github-opss <54327724+Github-opss@users.noreply.github.com>
2020-11-04 00:53:54 -0500

    Update program.sh

--- program.sh
+++ program.sh
@@ -1,18 +1,33 @@
 #! /bin/bash
 
+echo "Installing rpm fusion"
+dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  
+echo "Updating system"
+echo "fastestmirror=true" >> /etc/dnf/dnf.conf
+echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+dnf upgrade
+
 echo "Creating directories
 cd ~
 mkdir -p Downloads Documents Pictures Music 
 
 echo "Cloning git"
 git clone https://github.com/Github-opss/dotfiles.git
+cd dotfiles
 
 echo "Configuring i3"
+
+tar -xvf dracula.tar
+tar -xvf wallpaper.tar
+
 mv i3 i3status kitty rofi ~/.config
+mv dracula ~/.themes
+mv wallpaper ~
 
 echo "Installing theme"
-mkdir -p ~/.themes && tar -C ~/.themes -xvf dracula.tar
-
+mkdir -p ~/.themes &&  && cd ~ && 
 echo "Installing packages"
-dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi 
+dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi telegram-desktop
 
