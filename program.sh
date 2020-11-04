3a7bb4f9ac321a936739a8e45b2722db5e25a243
Github-opss <54327724+Github-opss@users.noreply.github.com>
2020-11-04 01:35:08 -0500

    Update program.sh

--- program.sh
+++ program.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
-
-echo "Installing rpm fusion"
+read -p "Enter your username" : n1
+echo "Welcome $n1"
+echo 
+#echo "Installing rpm fusion"
 dnf install \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-  
-echo "Updating system"
-echo "fastestmirror=true" >> /etc/dnf/dnf.conf
-echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
-dnf upgrade
+echo
+#echo "Updating system"
+#echo "fastestmirror=true" >> /etc/dnf/dnf.conf
+#echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+#dnf upgrade
 
-echo "Creating directories
+echo "Creating directories"
 cd ~
-mkdir -p Downloads Documents Pictures Music 
+mkdir -p Downloads Documents Pictures Music ~/.themes 
 
 echo "Cloning git"
 git clone https://github.com/Github-opss/dotfiles.git
@@ -22,12 +24,10 @@ echo "Configuring i3"
 tar -xvf dracula.tar
 tar -xvf wallpaper.tar
 
-mv i3 i3status kitty rofi ~/.config
-mv dracula ~/.themes
-mv wallpaper ~
+mv i3 i3status kitty rofi /home/$n1/.config
+mv dracula /home/$n1/.themes
+mv .wallpaper /home/$n1/
 
-echo "Installing theme"
-mkdir -p ~/.themes &&  && cd ~ && 
+
 echo "Installing packages"
-dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi telegram-desktop
-
+#dnf install brightnessctl kitty thunar feh lxpolkit lxappearance firefox firejail emacs rofi telegram-desktop
